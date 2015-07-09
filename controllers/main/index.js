f148d850b8ae95a6851314f33874232bd61e0b1f
elzafirox <enoy@roket.mx>
2015-07-09 14:52:16 -0600

    Archivos base

---
+++ controllers/main/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = module.exports = express();
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res){
+
+	var ipAddress;
+	var forwardedIpsStr = req.header('x-forwarded-for');
+	if (forwardedIpsStr) {
+		var forwardedIps = forwardedIpsStr.split(',');
+		ipAddress = forwardedIps[0];
+	}
+	if (!ipAddress) {
+		ipAddress = req.connection.remoteAddress;
+	}
+
+	res.render('index',{
+		title: 'Prueba de Deploy',
+		ip: ipAddress
+	});
+});
